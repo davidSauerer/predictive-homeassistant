d5cc09901acacbc738fe02a5bd37a3e4a8b1100b
Superbum <superbum@MacBook-Pro-von-Superbum.local>
2023-12-21 14:14:37 +0100

    Display entity on timeseries

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import argparse
 import psycopg2
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
+
+# Make NumPy printouts easier to read.
+np.set_printoptions(precision=3, suppress=True)
+
+
+def sql_enitity_states(entity):
+    return (f"SELECT s.state, s.last_updated_ts from states s JOIN states_meta es ON es.metadata_id = s.metadata_id "
+            f"WHERE es.entity_id LIKE '{entity}';")
+
+# Converts a unix timestamp into a date-time object
+def convert_timestamp_to_datetime(timestamp):
+    return datetime.utcfromtimestamp(float(timestamp))
+
+# Sort array to have the events in the right order
+def sort_events(unsorted):
+    return sorted(unsorted, key=lambda x: x[1])
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Personal information')
@@ -17,11 +38,42 @@ if __name__ == '__main__':
                             password=args.pwd,
                             port=args.port)
     print(conn)
+    print("TensorFlow version:", tf.__version__)
+
+    ##---gen trainings data
 
     cur = conn.cursor()
-    cur.execute("SELECT es.entity_id, s.state, s.last_updated_ts from states s JOIN states_meta es ON es.metadata_id = s.metadata_id WHERE es.entity_id LIKE 'input_boolean.sleeping';")
-    rows = cur.fetchall()
-    for row in rows:
-        print("State = ", row[1], "Updated", row[2])
+    cur.execute(sql_enitity_states("light.badschrabklampe"))
+    light = cur.fetchall()
+
+    cur.execute(sql_enitity_states("binary_sensor.badezimmer_motion"))
+    motion = cur.fetchall()
+
+    # sort array
+    sorted_array = np.array(sort_events(motion))
+
+    # Motion state
+    motion_state = sorted_array[:, 0]
+    # Time of change
+    motion_time = sorted_array[:, 1]
+
+    # Replace strings with numbers using list comprehension
+    string_to_number_mapping = {"off": 0, "on": 1}
+    motion_state_replaced = [string_to_number_mapping[item] for item in motion_state]
+    # Replace unix timestamp with date_time object
+    motion_time_date_time = [convert_timestamp_to_datetime(timestamp) for timestamp in motion_time]
+
+    # Plot the time series
+    plt.figure(figsize=(10, 6))
+    plt.plot(motion_time_date_time, motion_state_replaced, marker='o', linestyle='-')
+
+    # Add labels and title
+    plt.xlabel('Date')
+    plt.ylabel('Value')
+    plt.title('Time Series Example')
+
+    # Display the plot
+    plt.grid(True)
+    plt.show()
 
     conn.close()

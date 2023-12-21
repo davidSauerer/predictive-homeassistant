865bde870ca5f90e3ac1bf7f736323e094fe6eab
Superbum <superbum@MacBook-Pro-von-Superbum.local>
2023-12-21 19:29:34 +0100

    Allow filtering data by date and display data

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # predictive-homeassistant
+
+## Pre-Conditions
+* Homeassistant
+* External Postgres DB
+* Python installed
+
+## Use Case
+This piece of software should grep data of the Home Assistant Database and prepare it own a way that it could be processed
+by TensorFlow.
+
+## Example Arguments
+--name homeassistant_db --host 192.0.0.2 --user username --pass xxx --port 5432 --start 2023-12-05-15:30:00 --end 2023-12-21-18:30:00 --entities binary_sensor.badezimmer_motion light.badschrabklampe input_boolean.sleeping
+
+The Arguments --start and --end are optional, if they are not provided the tool will use all available data.
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import psycopg2
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-from datetime import datetime, timedelta
+from datetime import datetime
 
 # Make NumPy printouts easier to read.
 np.set_printoptions(precision=3, suppress=True)
@@ -11,15 +11,40 @@ np.set_printoptions(precision=3, suppress=True)
 
 def sql_enitity_states(entity):
     return (f"SELECT s.state, s.last_updated_ts from states s JOIN states_meta es ON es.metadata_id = s.metadata_id "
-            f"WHERE es.entity_id LIKE '{entity}';")
+            f"WHERE es.entity_id LIKE '{entity}' AND s.state not like 'unavailable';")
+
+
+def sql_enitity_states_with_start_stop(entity, start, end):
+    return (f"SELECT s.state, s.last_updated_ts from states s JOIN states_meta es ON es.metadata_id = s.metadata_id "
+            f"WHERE es.entity_id LIKE '{entity}' AND s.state not like 'unavailable' AND s.last_updated_ts >= "
+            f"{start} AND s.last_updated_ts <= {end};")
+
 
 # Converts a unix timestamp into a date-time object
 def convert_timestamp_to_datetime(timestamp):
     return datetime.utcfromtimestamp(float(timestamp))
 
+
 # Sort array to have the events in the right order
 def sort_events(unsorted):
     return sorted(unsorted, key=lambda x: x[1])
+
+
+def clean_data(array):
+    # sort array
+    sorted_array = np.array(sort_events(array))
+    # states
+    states = sorted_array[:, 0]
+    # Time of change
+    timestamps = sorted_array[:, 1]
+
+    # Replace strings with numbers using list comprehension
+    string_to_number_mapping = {"off": 0, "on": 1}
+    # print(motion_state)
+    state_replaced = [string_to_number_mapping[item] for item in states]
+    # Replace unix timestamp with date_time object
+    date_times = [convert_timestamp_to_datetime(timestamp) for timestamp in timestamps]
+    return date_times, state_replaced
 
 
 if __name__ == '__main__':
@@ -29,6 +54,12 @@ if __name__ == '__main__':
     parser.add_argument('--user', dest='user', type=str, help='The database user')
     parser.add_argument('--pass', dest='pwd', type=str, help='The database password')
     parser.add_argument('--port', dest='port', type=int, help='The database port')
+    parser.add_argument('--start', dest='start', type=str, help='Defines the start time of the data in the format '
+                                                                '%Y-%m-%d %H:%M:%S')
+    parser.add_argument('--end', dest='end', type=str, help='Defines the end time of the data in the format '
+                                                            '%Y-%m-%d %H:%M:%S')
+    parser.add_argument('--entities', dest='entities', nargs="+", type=str,
+                        help='List of entities which should be used as trainings data')
 
     args = parser.parse_args()
 
@@ -36,41 +67,38 @@ if __name__ == '__main__':
                             host=args.host,
                             user=args.user,
                             password=args.pwd,
-                            port=args.port)
+                            port=args.port,
+                            )
+
+    # Specify the format of the date string
+    date_format = "%Y-%m-%d-%H:%M:%S"
+
     print(conn)
     print("TensorFlow version:", tf.__version__)
 
-    ##---gen trainings data
+    plt.figure(figsize=(10, 6))
 
     cur = conn.cursor()
-    cur.execute(sql_enitity_states("light.badschrabklampe"))
-    light = cur.fetchall()
-
-    cur.execute(sql_enitity_states("binary_sensor.badezimmer_motion"))
-    motion = cur.fetchall()
-
-    # sort array
-    sorted_array = np.array(sort_events(motion))
-
-    # Motion state
-    motion_state = sorted_array[:, 0]
-    # Time of change
-    motion_time = sorted_array[:, 1]
-
-    # Replace strings with numbers using list comprehension
-    string_to_number_mapping = {"off": 0, "on": 1}
-    motion_state_replaced = [string_to_number_mapping[item] for item in motion_state]
-    # Replace unix timestamp with date_time object
-    motion_time_date_time = [convert_timestamp_to_datetime(timestamp) for timestamp in motion_time]
-
-    # Plot the time series
-    plt.figure(figsize=(10, 6))
-    plt.plot(motion_time_date_time, motion_state_replaced, marker='o', linestyle='-')
+    for e in args.entities:
+        print(args.start)
+        print(args.end)
+        if args.start is not None and args.end is not None:
+            print("if")
+            cur.execute(sql_enitity_states_with_start_stop(e, datetime.strptime(args.start, date_format).timestamp(),
+                                                           datetime.strptime(args.end, date_format).timestamp()))
+        else:
+            print("else")
+            cur.execute(sql_enitity_states(e))
+        states = cur.fetchall()
+        result_array1, result_array2 = clean_data(states)
+        # Plot the time series
+        plt.plot(result_array1, result_array2, marker='o', linestyle='-')
 
     # Add labels and title
     plt.xlabel('Date')
-    plt.ylabel('Value')
-    plt.title('Time Series Example')
+    plt.ylabel('State')
+    plt.title('Time Series')
+    # plt.legend()
 
     # Display the plot
     plt.grid(True)
